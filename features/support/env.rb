6f0ab8b35b3efc573923b341cb4e5fa2457c53c1
Kristaps Freimanis <kristaps.freimanis@testdevlab.com>
2017-12-18 23:20:34 +0200

    Updated with after hook

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,3 +2,7 @@ Before() do
   puts "Before hook. This will work before every test case!"
   @test_user = User.new('rest-api-automation@mailinator.com', 'Password!@£')
 end
+
+After() do
+  puts "Happens after a test scenario"
+end
