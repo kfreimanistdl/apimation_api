448d044f05a15ae9d7319eb1e9f586f89b23d3f0
Kristaps Freimanis <kristaps.freimanis@testdevlab.com>
2017-12-20 18:32:46 +0200

    Reporter update

--- reporter.rb
+++ reporter.rb
@@ -1,13 +1,15 @@
 require 'json'
 require_relative 'features/support/api_helper'
 
+build_number = ARGV[0]
+job_url = ARGV[1]
+
 thumbnail = { 'url' => 'https://images2.onionstatic.com/clickhole/3784/7/original/600.jpg' }
 fields = []
-fields.push( { 'name' => 'Author', 'value' => 'Iz gud' })
-fields.push( { 'name' => 'Position', 'value' => 'Engineering'})
+fields.push( { 'name' => 'Jenkins Job', 'value' => job_url })
+fields.push( { 'name' => 'Build number', 'value' => build_number.to_s})
 embed = []
-embed.push({ 'title' => 'Rich Content',
-                 'color' => 174592,
+embed.push({ 'color' => 174592,
                  'fields' => fields,
                  'thumbnail' => thumbnail})
 
